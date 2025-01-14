2155d14b54734d1290fc286a6addf11d42f9ed2d
Tomaz Pontes <tomazmppontes@gmail.com>
2025-01-14 19:06:00 -0300

    data: countries data

--- data/countries.ts
+++ data/countries.ts
@@ -13,377 +13,377 @@ export const countryData: Record<string, CountryData> = {
     id: "BR",
     name: "Brazil",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example2",
+    videoUrl: "https://www.youtube.com/watch?v=bil2kEOg-D8",
     views: 2000000,
-    visitDate: "2023-08-20"
+    visitDate: "2024-01-08"
   },
   IN: {
     id: "IN",
     name: "India",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example3",
+    videoUrl: "https://www.youtube.com/watch?v=h3PLizx9GIU",
     views: 1800000,
-    visitDate: "2023-09-10"
+    visitDate: "2023-10-12"
   },
   CZ: {
     id: "CZ",
     name: "Czech Republic",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example4",
+    videoUrl: "https://www.youtube.com/watch?v=evopWnMeCM0",
     views: 1200000,
-    visitDate: "2023-10-15"
+    visitDate: "2024-07-13"
   },
   AL: {
     id: "AL",
     name: "Albania",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example5",
+    videoUrl: "https://www.youtube.com/watch?v=hWDIHvBEzIY",
     views: 1300000,
-    visitDate: "2023-11-01"
+    visitDate: "2024-06-20"
   },
   DE: {
     id: "DE",
     name: "Germany",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example6",
+    videoUrl: "https://www.youtube.com/watch?v=gtATq8Zs7po",
     views: 1700000,
-    visitDate: "2023-11-15"
+    visitDate: "2024-06-16"
   },
   FR: {
     id: "FR",
     name: "France",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example7",
+    videoUrl: "https://www.youtube.com/watch?v=B4Q37W7vkm8",
     views: 1900000,
-    visitDate: "2023-12-01"
+    visitDate: "2023-06-05"
   },
   PT: {
     id: "PT",
     name: "Portugal",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example8",
+    videoUrl: "https://www.youtube.com/watch?v=R2v3Sui_9WA",
     views: 1400000,
-    visitDate: "2023-12-15"
+    visitDate: "2023-03-23"
   },
   TR: {
     id: "TR",
     name: "Turkey",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example9",
+    videoUrl: "https://www.youtube.com/watch?v=RHa2eBio4hI",
     views: 1600000,
-    visitDate: "2024-01-01"
+    visitDate: "2023-06-10"
   },
   ES: {
     id: "ES",
     name: "Spain",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example10",
+    videoUrl: "https://www.youtube.com/watch?v=n-KG_C7Sui4",
     views: 1750000,
-    visitDate: "2024-01-15"
+    visitDate: "2024-05-31"
   },
   NO: {
     id: "NO",
     name: "Norway",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example11",
+    videoUrl: "https://www.youtube.com/watch?v=_rgQQ89zND0",
     views: 1250000,
-    visitDate: "2024-02-01"
+    visitDate: "2024-07-03"
   },
   JP: {
     id: "JP",
     name: "Japan",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example12",
+    videoUrl: "https://www.youtube.com/watch?v=sZIPaQUc4uc",
     views: 2100000,
-    visitDate: "2024-02-15"
+    visitDate: "2023-07-14"
   },
   RO: {
     id: "RO",
     name: "Romania",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example13",
+    videoUrl: "https://www.youtube.com/watch?v=OAtqkkQ3eLE",
     views: 1350000,
-    visitDate: "2024-03-01"
+    visitDate: "2024-07-12"
   },
   GR: {
     id: "GR",
     name: "Greece",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example14",
+    videoUrl: "https://www.youtube.com/watch?v=NCA_O0oSItM",
     views: 1450000,
-    visitDate: "2024-03-15"
+    visitDate: "2024-07-08"
   },
   IT: {
     id: "IT",
     name: "Italy",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example15",
+    videoUrl: "https://www.youtube.com/watch?v=o595drmP55U",
     views: 1850000,
-    visitDate: "2024-04-01"
+    visitDate: "2024-06-23"
   },
   DK: {
     id: "DK",
     name: "Denmark",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example16",
+    videoUrl: "https://www.youtube.com/watch?v=vJiSTu7Za-c",
     views: 1300000,
-    visitDate: "2024-04-15"
+    visitDate: "2024-07-18"
   },
   AE: {
     id: "AE",
     name: "United Arab Emirates",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example17",
+    videoUrl: "https://www.youtube.com/watch?v=81GEDb9Qjj0",
     views: 1650000,
-    visitDate: "2024-05-01"
+    visitDate: "2022-12-08"
   },
   SA: {
     id: "SA",
     name: "Saudi Arabia",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example18",
+    videoUrl: "https://www.youtube.com/watch?v=q1trJRhH9SE",
     views: 1550000,
-    visitDate: "2024-05-15"
+    visitDate: "2024-02-02"
   },
   QA: {
     id: "QA",
     name: "Qatar",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example19",
+    videoUrl: "https://www.youtube.com/watch?v=b-k4Rj91uoQ",
     views: 1400000,
-    visitDate: "2024-06-01"
+    visitDate: "2022-12-06"
   },
   SE: {
     id: "SE",
     name: "Sweden",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example20",
+    videoUrl: "https://www.youtube.com/watch?v=vQK6F_Z9Y3A",
     views: 1350000,
-    visitDate: "2024-06-15"
+    visitDate: "2024-07-03"
   },
   BE: {
     id: "BE",
     name: "Belgium",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example21",
+    videoUrl: "https://www.youtube.com/watch?v=uDr3gD28tlg",
     views: 1250000,
-    visitDate: "2024-07-01"
+    visitDate: "2024-06-28"
   },
   NL: {
     id: "NL",
     name: "Netherlands",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example22",
+    videoUrl: "https://www.youtube.com/watch?v=Qbd0JhZUTjQ",
     views: 1450000,
-    visitDate: "2024-07-15"
+    visitDate: "2024-06-24"
   },
   KR: {
     id: "KR",
     name: "South Korea",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example23",
+    videoUrl: "https://www.youtube.com/watch?v=s7abxsByIJ8",
     views: 1750000,
-    visitDate: "2024-08-01"
+    visitDate: "2024-05-13"
   },
   KP: {
     id: "KP",
     name: "North Korea",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example24",
+    videoUrl: "https://youtu.be/Mra9vzMrtsw?si=Ecd3hXitcFI74TVj&t=169",
     views: 2200000,
-    visitDate: "2024-08-15"
+    visitDate: "2024-05-18"
   },
   BG: {
     id: "BG",
     name: "Bulgaria",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example25",
+    videoUrl: "https://www.youtube.com/watch?v=xhxL1h2RiEE",
     views: 1150000,
-    visitDate: "2024-09-01"
+    visitDate: "2024-07-11"
   },
   PL: {
     id: "PL",
     name: "Poland",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example26",
+    videoUrl: "https://www.youtube.com/watch?v=J6Hxw1Fzt1M",
     views: 1350000,
-    visitDate: "2024-09-15"
+    visitDate: "2024-07-02"
   },
   CH: {
     id: "CH",
     name: "Switzerland",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example27",
+    videoUrl: "https://www.youtube.com/watch?v=GaXdgQYLFw0",
     views: 1450000,
-    visitDate: "2024-10-01"
+    visitDate: "2024-07-06"
   },
   GB: {
     id: "GB",
     name: "United Kingdom",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example28",
+    videoUrl: "https://www.youtube.com/watch?v=QdZQKm6_V9Y",
     views: 1850000,
-    visitDate: "2024-10-15"
+    visitDate: "2022-09-20"
   },
   HU: {
     id: "HU",
     name: "Hungary",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example29",
+    videoUrl: "https://www.youtube.com/watch?v=VxDubYjDux8",
     views: 1250000,
-    visitDate: "2024-11-01"
+    visitDate: "2024-07-16"
   },
   AT: {
     id: "AT",
     name: "Austria",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example30",
+    videoUrl: "https://www.youtube.com/watch?v=f0DmRB1LGws",
     views: 1350000,
-    visitDate: "2024-11-15"
+    visitDate: "2024-07-16"
   },
   IE: {
     id: "IE",
     name: "Ireland",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example31",
+    videoUrl: "https://www.youtube.com/watch?v=vf_EczgiP1s",
     views: 1400000,
-    visitDate: "2024-12-01"
+    visitDate: "2024-07-19"
   },
   TH: {
     id: "TH",
     name: "Thailand",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example32",
+    videoUrl: "https://www.youtube.com/watch?v=oaRc-P3Na-w",
     views: 1650000,
-    visitDate: "2024-12-15"
+    visitDate: "2024-09-09"
   },
   PH: {
     id: "PH",
     name: "Philippines",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example33",
+    videoUrl: "https://www.youtube.com/watch?v=qWuJYrEZBkw",
     views: 1550000,
-    visitDate: "2025-01-01"
+    visitDate: "2024-09-11"
   },
   VN: {
     id: "VN",
     name: "Vietnam",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example34",
+    videoUrl: "https://www.youtube.com/watch?v=VTorUJcMYGg",
     views: 1450000,
-    visitDate: "2025-01-15"
+    visitDate: "2024-09-14"
   },
   KH: {
     id: "KH",
     name: "Cambodia",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example35",
+    videoUrl: "https://www.youtube.com/watch?v=yyNDJuMCxDc",
     views: 1350000,
-    visitDate: "2025-02-01"
+    visitDate: "2024-09-16"
   },
   SG: {
     id: "SG",
     name: "Singapore",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example36",
+    videoUrl: "https://www.youtube.com/watch?v=oZxOzl441qQ",
     views: 1750000,
-    visitDate: "2025-02-15"
+    visitDate: "2024-09-24"
   },
   MY: {
     id: "MY",
     name: "Malaysia",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example37",
+    videoUrl: "https://www.youtube.com/watch?v=Ws9hCaDuUWQ",
     views: 1650000,
-    visitDate: "2025-03-01"
+    visitDate: "2024-09-17"
   },
   ID: {
     id: "ID",
     name: "Indonesia",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example38",
+    videoUrl: "https://www.youtube.com/watch?v=9s6imDGjy08",
     views: 1850000,
-    visitDate: "2025-03-15"
+    visitDate: "2024-09-18"
   },
   AU: {
     id: "AU",
     name: "Australia",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example39",
+    videoUrl: "https://www.youtube.com/watch?v=C8Gqmu4p49s",
     views: 1950000,
-    visitDate: "2025-04-01"
+    visitDate: "2025-11-20"
   },
   NZ: {
     id: "NZ",
     name: "New Zealand",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example40",
+    videoUrl: "https://www.youtube.com/watch?v=eFz30QfDvwg",
     views: 1450000,
-    visitDate: "2025-04-15"
+    visitDate: "2024-11-23"
   },
   CO: {
     id: "CO",
     name: "Colombia",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example41",
+    videoUrl: "https://www.youtube.com/watch?v=1GBOYFAHRA0",
     views: 1550000,
-    visitDate: "2025-05-01"
+    visitDate: "2025-01-12"
   },
   GT: {
     id: "GT",
     name: "Guatemala",
     visited: true,
-    videoUrl: "https://youtube.com/watch?v=example42",
+    videoUrl: "https://www.youtube.com/watch?v=DGsqQlVTcGE",
     views: 1350000,
-    visitDate: "2025-05-15"
+    visitDate: "2025-01-15"
   },
   CL: {
     id: "CL",
     name: "Chile",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-06-01"
+    visitDate: "2025-01-02"
   },
   PE: {
     id: "PE",
     name: "Peru",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-06-15"
+    visitDate: "2025-01-02"
   },
   EC: {
     id: "EC",
     name: "Ecuador",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-07-01"
+    visitDate: "2025-01-02"
   },
   UY: {
     id: "UY",
     name: "Uruguay",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-07-15"
+    visitDate: "2025-01-02"
   },
   PY: {
     id: "PY",
     name: "Paraguay",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-08-01"
+    visitDate: "2025-01-02"
   },
   PA: {
     id: "PA",
     name: "Panama",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-08-15"
+    visitDate: "2025-01-02"
   },
   AR: {
     id: "AR",
     name: "Argentina",
     visited: false,
     confirmedVisit: true,
-    visitDate: "2024-09-01"
+    visitDate: "2025-01-02"
   },
 };
